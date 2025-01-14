fe2bae039ff1fee115abd7df5b6bb69dad1caa8b
Jon Doron <jond@wiz.io>
2025-01-14 16:03:49 +0200

    Engine: Replace memmem dependency with memchr
    Signed-off-by: Jon Doron <jond@wiz.io>

--- engine/src/searcher.rs
+++ engine/src/searcher.rs
@@ -1,5 +1,5 @@
-use memmem::Searcher;
 use std::mem::ManuallyDrop;
+use memchr::memmem;
 
 pub struct EmptySearcher;
 
@@ -18,7 +18,7 @@ pub struct TwoWaySearcher {
     // we don't want to tie it to the lifetime of `TwoWaySearcher`, since our data is heap-allocated
     // and is guaranteed to deref to the same address across moves of the container. Hence, we use
     // `static` as a substitute lifetime and it points to the same the data as `needle`.
-    searcher: ManuallyDrop<memmem::TwoWaySearcher<'static>>,
+    searcher: ManuallyDrop<memmem::Finder<'static>>,
 }
 
 // This is safe because we are only ever accessing `needle` mutably during `Drop::drop`
@@ -38,13 +38,13 @@ impl TwoWaySearcher {
 
         TwoWaySearcher {
             needle,
-            searcher: ManuallyDrop::new(memmem::TwoWaySearcher::new(needle_static)),
+            searcher: ManuallyDrop::new(memmem::Finder::new(needle_static)),
         }
     }
 
     #[inline]
     pub fn search_in(&self, haystack: &[u8]) -> bool {
-        self.searcher.search_in(haystack).is_some()
+        self.searcher.find(haystack).is_some()
     }
 }
 
